8ac607cda6abd81ba83221a8c230ecb644ece1d8
MarvinFos <develop.fos@gmail.com>
2026-04-02 10:12:49 -0400

    feat: lock app orientation to portrait mode
    Add SystemChrome.setPreferredOrientations call in main() to restrict the app to portrait orientation (both up and down). This prevents unwanted rotation on device orientation changes.

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
@@ -7,6 +8,10 @@ import 'core/push/push_notification_service.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await const PushNotificationService().initialize();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   runApp(const ProviderScope(child: ChambaApp()));
 }
